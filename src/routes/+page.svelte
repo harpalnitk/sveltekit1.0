55d55b6838836ce841a41bc542b26c09c29c7332
Harpal Singh1 <harpalnitk1@gmail.com>
2023-01-06 15:41:03 +0530

    added cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,54 @@
 <script>
+
+
     import Hero from "./Hero.svelte";
     import What from "./What.svelte";
+
+export let data;
+
+const {posts} = data;
+//OR
+// $: ({posts} = data);
+
+   
+    
 </script>
 
 <Hero/>
 <What/>
+
+<div class="container">
+    <!-- {#await getPosts()}
+    Loading...
+    {:then data} -->
+    <div class='grid'>
+    {#each posts as {title,url}}
+    <div>
+        <img src={url} alt={title}>
+        <p>{title}</p>
+    </div>
+    
+
+    
+    {/each}
+    </div>
+    <!-- {/await} -->
+</div>
+
+
+
+<style>
+    .grid{
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        gap: 2em;
+        padding: 2em 0;
+    }
+    img{
+        height: 300px;
+        width: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
+
